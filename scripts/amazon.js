89beff1dc26607a27467b995a97ccf1b9fd948a8
Karala1 <yyarala@gmail.com>
2025-07-31 20:29:26 +0400

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,7 +80,13 @@ document.querySelectorAll('.js-add-to-cart')
       });
       }
 
+      let cartQuantity = 0;
 
-      console.log(cart);
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
       });
   });
