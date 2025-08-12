08cecee59470247f3198b21f169782768c716c66
Karala1 <yyarala@gmail.com>
2025-08-12 19:03:51 +0400

    Save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,10 +5,12 @@ if(!cart){
    productId:
   'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
+  deliveryOptionsID: '1'
 }, {
   productId:
   '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionsID: '2'
 }];
 }
 
@@ -30,7 +32,8 @@ export function addToCart(productId){
       }else {  
         cart.push({
         productId: productId,
-        quantity: 1
+        quantity: 1,
+        deliveryOptionsID: '1'
       });
     }
 
