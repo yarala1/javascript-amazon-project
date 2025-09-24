e7ea1b9fc3b5c9a94da649cd89831a74d86f6848
Karala1 <yyarala@gmail.com>
2025-09-24 18:56:50 +0400

    Load producunts in before all hook

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,9 +1,16 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe('test suit: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    })
+  });
 
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
@@ -12,7 +19,6 @@ describe('test suit: renderOrderSummary', () => {
     <div class="js-order-summery"></div>
     <div class="js-payment-summary"></div>
     `;
-
 
   spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
@@ -26,6 +32,7 @@ describe('test suit: renderOrderSummary', () => {
       }]);
     });
     loadFromStorage();
+ 
     renderOrderSummary();
 });
 
