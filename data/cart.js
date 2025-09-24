2feafee82b64e0cc9b363c7955e6b747a3add545
Karala1 <yyarala@gmail.com>
2025-09-24 20:14:07 +0400

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -66,3 +66,16 @@ export function updateDeliveryOption(productId, deliveryOptionsID) {
 
   saveToStorage();
 }
+
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response)
+  fun();
+});
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+};
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,59 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-loadProducts(() => {
+
+// Loading all data in parallel ყველას ერთად ალოადებს ///
+Promise.all([
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+    });
+  }),
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+/// separate chaning of promises ცალც ცალკე ალოადებს///
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+  
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});  
+*/
+
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
