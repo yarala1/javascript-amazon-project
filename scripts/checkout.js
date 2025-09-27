24ebf58092335da5264d6e6c6b77d98e933fdb8c
Karala1 <yyarala@gmail.com>
2025-09-27 15:54:13 +0400

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,8 +5,22 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+async function loadPage(){
+  await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve(value3);
+    });
+  });
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
 
 // Loading all data in parallel ყველას ერთად ალოადებს ///
+/*
 Promise.all([
 loadProductsFetch(),
   new Promise((resolve) => {
@@ -20,6 +34,8 @@ loadProductsFetch(),
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
+
 
 /*
 /// separate chaning of promises ცალც ცალკე ალოადებს///
